241e94138a5daaebb379f1a9c5be81ba489d2a45
Severus7 <joshuasapz@gmail.com>
2022-03-06 10:23:14 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [text, setText] = useState("Not yet scanned");
+
+  const askForCameraPermission = () => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status == "granted");
+    })();
+  };
+
+  // Request Camera Permission
+  useEffect(() => {
+    askForCameraPermission();
+  }, []);
+
+  // What happens when we scan the bar code
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data);
+    console.log("Type: " + type + "\nData: " + data);
+  };
+
+  // Check permissions and return the screens
+  if (hasPermission === null) {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ fontSize: "24" }}>Welcome to iCITY</Text>
+        <Text>We empower cities by boosting their local economy</Text>
+        <Button
+          title={"Allow Camera"}
+          onPress={() => askForCameraPermission()}
+        />
+      </View>
+    );
+  }
+
+  // Return the View
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Welcome to iCITY</Text>
+      <Text>We empower cities by boosting their local economy</Text>
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={{ height: 400, width: 400 }}
+        />
+      </View>
+      <Text style={styles.maintext}>{text}</Text>
+
+      {scanned && (
+        <Button
+          title={"Scan again?"}
+          onPress={() => setScanned(false)}
+          color="tomator"
+        />
+      )}
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  maintext: {
+    fontSize: 16,
+    margin: 20,
+  },
+  barcodebox: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 300,
+    width: 300,
+    overflow: "hidden",
+    borderRadius: 30,
+    backgroundColor: "tomato",
   },
 });
---
+++ README.md
@@ -0,0 +1,17 @@
+# Hackfest-Manlilikha
+
+Gamification of Cities
+
+Team Members:
+
+SDG 11: Sustainable Cities and Communities
+
+Vision: To make cities sustainable by boosting their local economy
+
+Goal: Encourage citizens to support local businesses in their respective communities and increase civic engagement.
+
+Overview:
+
+Behavioral economics integrates psychology and economics to understand how humans make decisions and the reason for their behavior. If city officials can discern the underlying principles of what drives their citizens’ behavior, they may encourage and reward behaviors that are consistent with their objectives. In order for that to be possible, we decided to gamify city interactions. Gamification is the technique of incorporating game-like elements into non-game environments to stimulate participation.
+
+Our solution will have a huge economic impact and increase civic engagement. This is a win-win situation for both local business owners and citizens. By supporting local businesses, residents of a particular city will be rewarded. This will boost the city economy which will make it more sustainable and resilient. Rewards will be in the form of points, cashback rewards, or freebies from the local businesses. Aside from that, citizens will also be rewarded whenever they participate in local events, by expressing their ideas and opinions, and giving feedback to the local government.
